27f0c34becbc83d4f5b58cab4a38186eef7bb1ca
haominshi <haominshi@gmail.com>
2018-05-09 22:04:56 -0500

    added menu

--- Convertor/app/src/main/java/com/haomins/www/convertor/MainActivity.java
+++ Convertor/app/src/main/java/com/haomins/www/convertor/MainActivity.java
@@ -2,6 +2,9 @@ package com.haomins.www.convertor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.type_of_converstion_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_length:
+				Toast.makeText(this, "Length", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_area:
+				Toast.makeText(this, "Area", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_EE:
+				Toast.makeText(this, "EE", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_weight:
+				Toast.makeText(this,"Weight", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_temp:
+				Toast.makeText(this,"Temp", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_quick:
+				Toast.makeText(this,"Quick launch", Toast.LENGTH_SHORT).show();
+				return true;
+			default:
+				return true;
+		}
 	}
 }
